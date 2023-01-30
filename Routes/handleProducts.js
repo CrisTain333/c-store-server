2ffc746dca042b69cdb3a90de0e55c60f925bb7f
Sukanta Das <arnobh84@gmail.com>
2023-01-30 15:41:31 +0600

    category api & product api changed

--- Routes/handleProducts.js
+++ Routes/handleProducts.js
@@ -5,10 +5,53 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@pra
 const client = new MongoClient(uri);
 const productCollection = client.db("cStore").collection("products");
 
-handleProducts.get("/", async (req, res) => {
-  const filter = {};
-  const result = await productCollection.find(filter).toArray();
-  res.status(200).send(result);
+// Get Category
+handleProducts.get("/category", async (req, res, next) => {
+  try {
+    const filter = {};
+    const result = await productCollection.find(filter).toArray();
+    const category = result.map((x) => {
+      return x.category;
+    });
+    let unique = category.filter((v, i, a) => a.indexOf(v) === i);
+    res.status(200).send(unique);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
 });
+
+// Get Products
+handleProducts.get("/:category", async (req, res, next) => {
+  try {
+    const category = req.query.category;
+    let filter = {};
+    if (category === "all") {
+      const result = await productCollection.find({}).toArray();
+      res.status(200).send(result);
+    }
+    filter = { category: category };
+    console.log(filter);
+    const result = await productCollection.find(filter).toArray();
+    res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
+
+// handleProducts.get("/:category", async (req, res) => {
+//   const category = req.query.category;
+//   console.log(category);
+//   const filter = {};
+//   const result = await productCollection.find(filter).toArray();
+//   res.status(200).send(result);
+// });
+
+// handleProducts.get("/:category", async (req, res) => {
+//   const filter = {};
+//   const result = await productCollection.find(filter).toArray();
+//   res.status(200).send(result);
+// });
 
 module.exports = handleProducts;
