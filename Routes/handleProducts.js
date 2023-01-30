804b9f765879ab3073ee796d5a6412c52485e399
Sukanta Das <arnobh84@gmail.com>
2023-01-30 13:09:39 +0600

    product api added

--- Routes/handleProducts.js
+++ Routes/handleProducts.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const handleProducts = express.Router();
+const { MongoClient, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@practicebaba.aon4ndq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+const productCollection = client.db("cStore").collection("products");
 
 handleProducts.get("/", async (req, res) => {
-  res.send("product home route working");
+  const filter = {};
+  const result = await productCollection.find(filter).toArray();
+  res.status(200).send(result);
 });
 
 module.exports = handleProducts;
--- index.js
+++ index.js
@@ -12,15 +12,13 @@ const productRoute = require("./Routes/handleProducts");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@practicebaba.aon4ndq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
-const productCollection = client.db("cStore").collection("products");
 
 app.get("/", (req, res) => {
   res.send("C-store Server On Fire");
 });
 
 // Product Routes
-
-app.use("/product", productRoute);
+app.use("/products", productRoute);
 
 app.listen(PORT, () => {
   console.log(`Betacom server Running On Port ${PORT} `);
