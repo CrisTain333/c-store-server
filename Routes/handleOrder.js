87b55e5e7ccfdd982324df2a1a76c3635c1e0013
Sukanta Das <arnobh84@gmail.com>
2023-02-01 21:03:42 +0600

    quantity updated method added

--- Routes/handleOrder.js
+++ Routes/handleOrder.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const handleOrder = express.Router();
+const { MongoClient, ObjectId } = require("mongodb");
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@practicebaba.aon4ndq.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+const cartCollection = client.db("cStore").collection("cart");
 
-handleOrder.get("/", async (req, res, next) => {
-  res.send("runing");
+handleOrder.post("/", async (req, res, next) => {
+  const carts = req.body;
+  //   const result = await cartCollection.insertOne(cart);
+  const checkAllCart = await cartCollection
+    .find({
+      email: carts.email,
+    })
+    .toArray();
+
+  const checkAllReadyExits = checkAllCart.find(
+    (cart) => cart.productId === carts.productId
+  );
+
+  if (checkAllReadyExits) {
+    const quantity = checkAllReadyExits.productQuantity;
+    console.log(quantity);
+    const filter = { email: checkAllReadyExits.email };
+    const updatedDoc = {
+      $set: {
+        productQuantity: quantity + 1,
+      },
+    };
+    const result = await cartCollection.updateOne(filter, updatedDoc);
+    console.log(result);
+  } else {
+    console.log(false);
+  }
+  //   console.log(checkAllReadyExits);
+  res.send("ok");
 });
 
 module.exports = handleOrder;
