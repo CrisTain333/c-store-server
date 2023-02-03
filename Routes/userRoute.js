cb9fdc8b4ff4bcbc1ac538b75e426913f50270ca
Sukanta Das <arnobh84@gmail.com>
2023-02-03 18:56:51 +0600

    error fixed

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -8,6 +8,27 @@ const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 const userCollection = client.db("cStore").collection("user");
 
+//Get User
+userRoute.get("/:email", async (req, res, next) => {
+  try {
+    const email = req.params.email;
+    console.log(email);
+    const user = await userCollection.findOne({ email: email });
+    console.log(user);
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    if (user) {
+      res.status(200).json({ status: 200, token: token, user: user });
+    } else {
+      res.status(404).json({ status: 401, message: "User Not Found" });
+    }
+  } catch (error) {
+    console.log(error);
+    next();
+  }
+});
+
 // saveUser
 userRoute.post("/", async (req, res, next) => {
   try {
@@ -32,25 +53,6 @@ userRoute.post("/", async (req, res, next) => {
   } catch (error) {
     console.log(error);
     res.status(401).json({ errorMessage: "Authentication failed" });
-    next();
-  }
-});
-
-//Get User
-userRoute.get("/:email", async (req, res, next) => {
-  try {
-    const email = req.params.email;
-    const user = await userCollection.findOne({ email: email });
-    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
-    });
-    if (user) {
-      res.status(200).json({ status: 200, token: token, user: user });
-    } else {
-      res.status(404).json({ status: 401, message: "User Not Found" });
-    }
-  } catch (error) {
-    console.log(error);
     next();
   }
 });
