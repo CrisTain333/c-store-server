0724ad8a717eeb9e619edb8519ba1cd46f1b790a
Sukanta Das <arnobh84@gmail.com>
2023-02-04 20:12:32 +0600

    ssl installed

--- Routes/handlePayment.js
+++ Routes/handlePayment.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const handlePayment = express.Router();
+const SSLCommerzPayment = require("sslcommerz-lts");
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASSWORD;
+const is_live = false; //true for live, false for sandbox
 
 handlePayment.get("/", async (req, res) => {
   res.json({ status: 400 });
